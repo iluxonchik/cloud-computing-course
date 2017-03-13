fb5da94f39d8f2c83d09ae2f2fd86ac3709642dc
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-03-13 23:06:26 +0000

    feat: init lab 02 files

---
+++ lab_02/pin/source/tools/ManualExamples/pinatrace.cpp
@@ -0,0 +1,156 @@
+/*BEGIN_LEGAL
+Intel Open Source License
+
+Copyright (c) 2002-2016 Intel Corporation. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.  Redistributions
+in binary form must reproduce the above copyright notice, this list of
+conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.  Neither the name of
+the Intel Corporation nor the names of its contributors may be used to
+endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE INTEL OR
+ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+END_LEGAL */
+/*
+ *  This file contains an ISA-portable PIN tool for tracing memory accesses.
+ */
+
+#include <stdio.h>
+#include "pin.H"
+
+/*
+  If you get in the output two consecutive rows with the same data, except in
+  one you have a R and in the oter one you have a W, this means you've encountered
+  a "complex" instruction.
+
+  For example: ADD R[1234], AX
+
+  Output format:
+    PC: R|w addr_in_memory_being_accessed
+
+
+
+  Ex:
+    0x7fde3c448c75: R 0x7fde3c666e68
+    0x7fde3c448c75: W 0x7fde3c666e68
+
+*/
+
+
+FILE * trace;
+
+// Print a memory read record
+VOID RecordMemRead(VOID * ip, VOID * addr)
+{
+    fprintf(trace,"%p: R %p\n", ip, addr);
+}
+
+// Print a memory write record
+VOID RecordMemWrite(VOID * ip, VOID * addr)
+{
+    fprintf(trace,"%p: W %p\n", ip, addr);
+}
+
+// Is called for every instruction and instruments reads and writes
+VOID Instruction(INS ins, VOID *v)
+{
+    // Instruments memory accesses using a predicated call, i.e.
+    // the instrumentation is called iff the instruction will actually be executed.
+    //
+    // On the IA-32 and Intel(R) 64 architectures conditional moves and REP
+    // prefixed instructions appear as predicated instructions in Pin.
+
+    /*
+      How many operands does instruction "ins" have?
+      Ex:
+        ADD AX, BX has 2 operands
+    */
+    UINT32 memOperands = INS_MemoryOperandCount(ins);
+
+    // Iterate over each memory operand of the instruction.
+    for (UINT32 memOp = 0; memOp < memOperands; memOp++)
+    {
+        /*
+          if (is read operation)
+        */
+        if (INS_MemoryOperandIsRead(ins, memOp))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_END);
+        }
+        // Note that in some architectures a single memory operand can be
+        // both read and written (for instance incl (%eax) on IA-32)
+        // In that case we instrument it once for read and once for write.
+
+        /*
+          Is this instruction a write?
+
+          mepOp - memory operand that I'm using.
+            Ex: ADD R[1234] -> here the memOp is 1234
+        */
+        if (INS_MemoryOperandIsWritten(ins, memOp))
+        {
+            INS_InsertPredicatedCall(
+                ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
+                IARG_INST_PTR,
+                IARG_MEMORYOP_EA, memOp,
+                IARG_END);
+        }
+    }
+}
+
+VOID Fini(INT32 code, VOID *v)
+{
+    fprintf(trace, "#eof\n");
+    fclose(trace);
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
+INT32 Usage()
+{
+    PIN_ERROR( "This Pintool prints a trace of memory addresses\n"
+              + KNOB_BASE::StringKnobSummary() + "\n");
+    return -1;
+}
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
+
+int main(int argc, char *argv[])
+{
+    if (PIN_Init(argc, argv)) return Usage();
+
+    trace = fopen("pinatrace.out", "w");
+
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Never returns
+    PIN_StartProgram();
+
+    return 0;
+}
